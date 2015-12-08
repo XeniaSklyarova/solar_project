bdf39e7b1869b37d273cbefd54d5576eea436905
XeniaSklyarova <sklyarovtmk68@rambler.ru>
2015-12-08 11:33:13 +0300

    Update solar_model.py

--- solar_model.py
+++ solar_model.py
@@ -4,26 +4,13 @@
 gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
 
-
-def calculate_force(body, space_objects):
-    """Вычисляет силу, действующую на тело.
-    Параметры:
-    **body** — тело, для которого нужно вычислить дейстующую силу.
-    **space_objects** — список объектов, которые воздействуют на тело.
-    **rc** - расстояние до ц.м.сиситемы
-    **rcx** - расстояние до ц.м.с. по оси х
-    **rcy** - расстояние до ц.м.с. по оси у
-    **mrcx(y)** - масса да на координату помноженная
-    """
-
-    body.Fx = body.Fy = 0
-    body.Fx = body.Fy = 0
+body.Fx = body.Fy = 0
     for obj in space_objects:
         if body == obj:
-            continue
+            continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += body.m * obj.m * G * (body.x-obj.x) * (r)**-3
-        body.Fy += body.m * obj.m * G * (body.y-obj.y) * (r)**-3
+        body.Fx += (gravitational_constant*body.m*obj.m/(r**2))*(abs(body.x-obj.x)/r)  
+        body.Fy += (-gravitational_constant*body.m*obj.m/(r**2))*(abs(body.y-obj.y)/r)  
 
 
 
